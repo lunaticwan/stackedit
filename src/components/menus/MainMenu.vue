1a5efa6ed7010793673376168c333c1052551bb4
lunaticwan <3921422+lunaticwan@users.noreply.github.com>
2026-09-06 23:49:16 +0000

    Prepare StackEdit for air-gapped standalone static site deployment

--- src/components/menus/MainMenu.vue
+++ src/components/menus/MainMenu.vue
@@ -1,59 +1,17 @@
 <template>
   <div class="side-bar__panel side-bar__panel--menu">
     <div class="side-bar__info">
-      <div class="menu-entry menu-entry--info flex flex--row flex--align-center" v-if="loginToken">
-        <div class="menu-entry__icon menu-entry__icon--image">
-          <user-image :user-id="userId"></user-image>
-        </div>
-        <span>Signed in as <b>{{loginToken.name}}</b>.</span>
-      </div>
-      <div class="menu-entry menu-entry--info flex flex--row flex--align-center" v-if="syncToken">
-        <div class="menu-entry__icon menu-entry__icon--image">
-          <icon-provider :provider-id="currentWorkspace.providerId"></icon-provider>
-        </div>
-        <span v-if="currentWorkspace.providerId === 'googleDriveAppData'">
-          <b>{{currentWorkspace.name}}</b> synced with your Google Drive app data folder.
-        </span>
-        <span v-else-if="currentWorkspace.providerId === 'googleDriveWorkspace'">
-          <b>{{currentWorkspace.name}}</b> synced with a <a :href="workspaceLocationUrl" target="_blank">Google Drive folder</a>.
-        </span>
-        <span v-else-if="currentWorkspace.providerId === 'couchdbWorkspace'">
-          <b>{{currentWorkspace.name}}</b> synced with a <a :href="workspaceLocationUrl" target="_blank">CouchDB database</a>.
-        </span>
-        <span v-else-if="currentWorkspace.providerId === 'githubWorkspace'">
-          <b>{{currentWorkspace.name}}</b> synced with a <a :href="workspaceLocationUrl" target="_blank">GitHub repo</a>.
-        </span>
-        <span v-else-if="currentWorkspace.providerId === 'gitlabWorkspace'">
-          <b>{{currentWorkspace.name}}</b> synced with a <a :href="workspaceLocationUrl" target="_blank">GitLab project</a>.
-        </span>
-      </div>
-      <div class="menu-entry menu-entry--info flex flex--row flex--align-center" v-else>
+      <div class="menu-entry menu-entry--info flex flex--row flex--align-center">
         <div class="menu-entry__icon menu-entry__icon--disabled">
           <icon-sync-off></icon-sync-off>
         </div>
-        <span><b>{{currentWorkspace.name}}</b> not synced.</span>
+        <span><b>{{currentWorkspace.name}}</b> (Standalone local mode).</span>
       </div>
     </div>
-    <menu-entry v-if="!loginToken" @click.native="signin">
-      <icon-login slot="icon"></icon-login>
-      <div>Sign in with Google</div>
-      <span>Sync your main workspace and unlock functionalities.</span>
-    </menu-entry>
     <menu-entry @click.native="setPanel('workspaces')">
       <icon-database slot="icon"></icon-database>
       <div><div class="menu-entry__label menu-entry__label--count" v-if="workspaceCount">{{workspaceCount}}</div> Workspaces</div>
       <span>Switch to another workspace.</span>
-    </menu-entry>
-    <hr>
-    <menu-entry @click.native="setPanel('sync')">
-      <icon-sync slot="icon"></icon-sync>
-      <div><div class="menu-entry__label menu-entry__label--count" v-if="syncLocationCount">{{syncLocationCount}}</div> Synchronize</div>
-      <span>Sync your files in the Cloud.</span>
-    </menu-entry>
-    <menu-entry @click.native="setPanel('publish')">
-      <icon-upload slot="icon"></icon-upload>
-      <div><div class="menu-entry__label menu-entry__label--count" v-if="publishLocationCount">{{publishLocationCount}}</div>Publish</div>
-      <span>Export your files to the web.</span>
     </menu-entry>
     <menu-entry @click.native="setPanel('history')">
       <icon-history slot="icon"></icon-history>
@@ -89,11 +47,6 @@
       <div><div class="menu-entry__label menu-entry__label--count">{{badgeCount}}/{{featureCount}}</div> Badges</div>
       <span>List application features and earned badges.</span>
     </menu-entry>
-    <menu-entry @click.native="accounts">
-      <icon-key slot="icon"></icon-key>
-      <div><div class="menu-entry__label menu-entry__label--count">{{accountCount}}</div> Accounts</div>
-      <span>Manage access to your external accounts.</span>
-    </menu-entry>
     <menu-entry @click.native="templates">
       <icon-code-braces slot="icon"></icon-code-braces>
       <div><div class="menu-entry__label menu-entry__label--count">{{templateCount}}</div> Templates</div>
@@ -123,46 +76,21 @@
 <script>
 import { mapGetters, mapActions } from 'vuex';
 import MenuEntry from './common/MenuEntry';
-import providerRegistry from '../../services/providers/common/providerRegistry';
-import UserImage from '../UserImage';
-import googleHelper from '../../services/providers/helpers/googleHelper';
-import syncSvc from '../../services/syncSvc';
-import userSvc from '../../services/userSvc';
 import store from '../../store';
 
 export default {
   components: {
     MenuEntry,
-    UserImage,
   },
   computed: {
     ...mapGetters('workspace', [
       'currentWorkspace',
-      'syncToken',
-      'loginToken',
     ]),
-    userId() {
-      return userSvc.getCurrentUserId();
-    },
-    workspaceLocationUrl() {
-      const provider = providerRegistry.providersById[this.currentWorkspace.providerId];
-      return provider.getWorkspaceLocationUrl(this.currentWorkspace);
-    },
     workspaceCount() {
       return Object.keys(store.getters['workspace/workspacesById']).length;
     },
-    syncLocationCount() {
-      return Object.keys(store.getters['syncLocation/currentWithWorkspaceSyncLocation']).length;
-    },
-    publishLocationCount() {
-      return Object.keys(store.getters['publishLocation/current']).length;
-    },
     templateCount() {
       return Object.keys(store.getters['data/allTemplatesById']).length;
-    },
-    accountCount() {
-      return Object.values(store.getters['data/tokensByType'])
-        .reduce((count, tokensBySub) => count + Object.values(tokensBySub).length, 0);
     },
     badgeCount() {
       return store.getters['data/allBadges'].filter(badge => badge.isEarned).length;
@@ -175,14 +103,6 @@ export default {
     ...mapActions('data', {
       setPanel: 'setSideBarPanel',
     }),
-    async signin() {
-      try {
-        await googleHelper.signin();
-        syncSvc.requestSync();
-      } catch (e) {
-        // Cancel
-      }
-    },
     async fileProperties() {
       try {
         await store.dispatch('modal/open', 'fileProperties');
--- src/components/menus/WorkspacesMenu.vue
+++ src/components/menus/WorkspacesMenu.vue
@@ -12,31 +12,12 @@
         <div class="workspace__name"><div class="menu-entry__label" v-if="currentWorkspace === workspace">current</div>{{workspace.name}}</div>
       </menu-entry>
     </div>
-    <hr>
-    <menu-entry @click.native="addCouchdbWorkspace">
-      <icon-provider slot="icon" provider-id="couchdbWorkspace"></icon-provider>
-      <span>Add a <b>CouchDB</b> workspace</span>
-    </menu-entry>
-    <menu-entry @click.native="addGithubWorkspace">
-      <icon-provider slot="icon" provider-id="githubWorkspace"></icon-provider>
-      <span>Add a <b>GitHub</b> workspace</span>
-    </menu-entry>
-    <menu-entry @click.native="addGitlabWorkspace">
-      <icon-provider slot="icon" provider-id="gitlabWorkspace"></icon-provider>
-      <span>Add a <b>GitLab</b> workspace</span>
-    </menu-entry>
-    <menu-entry @click.native="addGoogleDriveWorkspace">
-      <icon-provider slot="icon" provider-id="googleDriveWorkspace"></icon-provider>
-      <span>Add a <b>Google Drive</b> workspace</span>
-    </menu-entry>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import MenuEntry from './common/MenuEntry';
-import googleHelper from '../../services/providers/helpers/googleHelper';
-import gitlabHelper from '../../services/providers/helpers/gitlabHelper';
 import store from '../../store';
 
 export default {
@@ -53,39 +34,6 @@ export default {
     },
   },
   methods: {
-    async addCouchdbWorkspace() {
-      try {
-        store.dispatch('modal/open', {
-          type: 'couchdbWorkspace',
-        });
-      } catch (e) { /* Cancel */ }
-    },
-    async addGithubWorkspace() {
-      try {
-        store.dispatch('modal/open', {
-          type: 'githubWorkspace',
-        });
-      } catch (e) { /* Cancel */ }
-    },
-    async addGitlabWorkspace() {
-      try {
-        const { serverUrl, applicationId } = await store.dispatch('modal/open', { type: 'gitlabAccount' });
-        const token = await gitlabHelper.addAccount(serverUrl, applicationId);
-        store.dispatch('modal/open', {
-          type: 'gitlabWorkspace',
-          token,
-        });
-      } catch (e) { /* Cancel */ }
-    },
-    async addGoogleDriveWorkspace() {
-      try {
-        const token = await googleHelper.addDriveAccount(true);
-        store.dispatch('modal/open', {
-          type: 'googleDriveWorkspace',
-          token,
-        });
-      } catch (e) { /* Cancel */ }
-    },
     manageWorkspaces() {
       try {
         store.dispatch('modal/open', 'workspaceManagement');
